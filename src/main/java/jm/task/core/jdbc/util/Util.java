c71f76e55388868ff9aae434185656cf1f536645
aroslavcekaskin <jarikkickass@mail.ru>
2025-02-26 19:09:33 +0500

    Add new DB Pass

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,7 +8,7 @@ public class Util {
 
     private static final String URL = "jdbc:mysql://localhost:3306/jdbctest";
     private static final String USER = "root";
-    private static final String PASSWORD = "DBroot";
+    private static final String PASSWORD = "dbrootdb";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
     public static Connection getConnection() {
